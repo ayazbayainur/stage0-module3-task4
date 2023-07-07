31c1f3034bc0beb9cea5dd349c3c47172e9c1d39
Ainur <ayazbay.ainur@mail.ru>
2023-07-07 21:47:22 +0600

    2nd commit

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -3,7 +3,9 @@ package school.mjc.stage0.module3.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         double a = 1, b = 2;
-        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b)/(2));
+        double right = (9 * a * a - 5 * b + 2 + a - 7);
+        double left = (a + b - 4 * a * b)/2;
+        double formula = right * left;
         System.out.println(formula);
     }
 }
